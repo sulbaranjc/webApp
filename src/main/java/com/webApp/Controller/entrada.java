07ec976eea01fdf2247d83ffb94e7d945343f6d6
Juan Carlos Sulbaram <sulbaranjc@gmail.com>
2024-03-12 15:50:47 +0100

    primer commit

--- src/main/java/com/webApp/Controller/entrada.java
+++ src/main/java/com/webApp/Controller/entrada.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping("/webapp")
 public class entrada {
-    @GetMapping("/index")
+    @GetMapping({"/", "/index"})
     public String index(Model model) {
         GestorEjemplo ge = new GestorEjemplo ();
         String valorfinal="./index";
